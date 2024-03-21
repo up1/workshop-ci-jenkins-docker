f102ebeacd55496b86547f13bc21bbe5fb92858e
Somkiat Puisungnoen <somkiat.p@gmail.com>
2024-03-21 10:49:43 +0700

    Update data.sql

--- database/data.sql
+++ database/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO merchants(name, email) VALUES ('name1', 'name1@email.com');
-INSERT INTO merchants(name, email) VALUES ('name2', 'name2@email.com');
-INSERT INTO merchants(name, email) VALUES ('name3', 'name3@email.com');
+INSERT INTO merchants(name, email) VALUES ('name11', 'name1@email.com');
+INSERT INTO merchants(name, email) VALUES ('name22', 'name2@email.com');
+INSERT INTO merchants(name, email) VALUES ('name33', 'name3@email.com');
