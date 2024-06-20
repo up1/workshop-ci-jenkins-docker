2a9409c99c2ba08cab6a4b003cfc75aa16e4074f
up1 <somkiat.p@gmail.com>
2024-06-20 08:47:52 +0700

    Add opentemetry

--- hello-dotnet/Program.cs
+++ hello-dotnet/Program.cs
@@ -1,3 +1,8 @@
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add logging
+builder.Logging
+    .AddOpenTelemetry(options => options.AddOtlpExporter())
+    .AddConsole();
+
+// Add tracing and metric
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracerBuilder => tracerBuilder
+        .AddAspNetCoreInstrumentation()
+        .AddConsoleExporter()
+        .AddOtlpExporter())
+    .WithMetrics(meterBuilder => meterBuilder
+        .AddAspNetCoreInstrumentation()
+        .AddConsoleExporter()
+        .AddOtlpExporter());
 
 var app = builder.Build();
 
---
+++ hello-dotnet/README.md
@@ -0,0 +1,18 @@
+# .Net 7 + Opentelemetry
+
+## Install packages
+```
+dotnet add package OpenTelemetry.Extensions.Hosting
+dotnet add package OpenTelemetry.Instrumentation.AspNetCore
+dotnet add package OpenTelemetry.Exporter.Console
+dotnet add package OpenTelemetry.Exporter.OpenTelemetryProtocol
+```
+
+## Run
+```
+export OTEL_SERVICE_NAME=demo-service
+export OTEL_RESOURCE_ATTRIBUTES=deployment.environment=DEV,service.namespace=demo,service.version=1.0,service.instance.id=server01
+
+dotnet restore
+dotnet run
+```
