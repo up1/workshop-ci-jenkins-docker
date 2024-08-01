797991b9b215161c7a100fb8900bd8484e0aa235
up1 <somkiat.p@gmail.com>
2024-08-01 15:59:32 +0700

    Delete data from 3 to 2 rows

--- database/data.sql
+++ database/data.sql
@@ -1,3 +1,2 @@
 INSERT INTO merchants(name, email) VALUES ('name11', 'name1@email.com');
 INSERT INTO merchants(name, email) VALUES ('name22', 'name2@email.com');
-INSERT INTO merchants(name, email) VALUES ('name33', 'name3@email.com');
