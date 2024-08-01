1f910d1be328145130b30675d3a833c369befb05
up1 <somkiat.p@gmail.com>
2024-08-01 15:35:25 +0700

    Demo with php + mysql

---
+++ php/dump/data.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `Person` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+INSERT INTO `Person` (`id`, `name`) VALUES
+(1, 'Person 01'),
+(2, 'Person 02'),
+(3, 'Person 03'),
+(4, 'Person 04'),
+(5, 'Person 05');
---
+++ php/web/index.php
@@ -0,0 +1,44 @@
+<html>
+ <head>
+  <title>Hello...</title>
+
+  <meta charset="utf-8"> 
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+</head>
+<body>
+    <div class="container">
+    <?php echo "<h1>Hi! I'm happy</h1>"; ?>
+
+    <?php
+
+    $conn = mysqli_connect('db', 'user', 'test', "demodb");
+
+
+    $query = 'SELECT * From Person';
+    $result = mysqli_query($conn, $query);
+
+    echo '<table class="table table-striped">';
+    echo '<thead><tr><th></th><th>id</th><th>name</th></tr></thead>';
+    while($value = $result->fetch_array(MYSQLI_ASSOC)){
+        echo '<tr>';
+        echo '<td><a href="#"><span class="glyphicon glyphicon-search"></span></a></td>';
+        foreach($value as $element){
+            echo '<td>' . $element . '</td>';
+        }
+
+        echo '</tr>';
+    }
+    echo '</table>';
+
+    $result->close();
+
+    mysqli_close($conn);
+
+    ?>
+    </div>
+</body>
+</html>
